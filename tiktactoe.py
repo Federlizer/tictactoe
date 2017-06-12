8c5185a0d03f6a0d94b9b3da74caf13db2645e84
Nikola Velichkov <nikola@linux-ixv3.suse>
2017-06-12 23:16:21 +0300

    Upgraded even more`

--- tiktactoe.py
+++ tiktactoe.py
@@ -1,23 +1,28 @@
 cell_data = {
-	1: " ",
+	1: "X",
 	2: " ",
-	3: " ",
+	3: "O",
 	4: " ",
 	5: " ",
 	6: " ",
 	7: " ",
 	8: " ",
-	9: " "	
+	9: " "
 }
 
 
 
 
 def draw_grid():
+	for x in cell_data.keys():
+		print(x)	
+	
 	border = "+---+---+---+\n"
-	cell_column = "|  |  |  |"
-	print(border + border)
+	cell_column_one = "| " + cell_data[1] + " | " + cell_data[2] + " " + template + cell_data[3] + " |\n"
 
+	total = border + cell_column_one + border
+
+	print(total)
 
 def check_cells():
 	pass
