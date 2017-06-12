2c0ec373bbd8a4db0a76cc2fc92c87a909799813
Nikola Velichkov <nikola@linux-ixv3.suse>
2017-06-12 22:47:30 +0300

    Improved the tiktactoe game

---
+++ tiktactoe.py
@@ -0,0 +1,28 @@
+cell_data = {
+	1: " ",
+	2: " ",
+	3: " ",
+	4: " ",
+	5: " ",
+	6: " ",
+	7: " ",
+	8: " ",
+	9: " "	
+}
+
+
+
+
+def draw_grid():
+	border = "+---+---+---+\n"
+	cell_column = "|  |  |  |"
+	print(border + border)
+
+
+def check_cells():
+	pass
+
+def something_that_changes_the_data_in_():
+	pass
+
+draw_grid()
